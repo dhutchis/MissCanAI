c815d44aed5108680629be37d5c4ad25ed4e5fa4
Dylan Hutchison <dhutchis@stevens.edu>
2012-09-06 19:46:55 -0400

    Almost stable
    Took away extra features.  It almost works except for an object.Error
    Access violation

--- src/MissCanAI.d
+++ src/MissCanAI.d
@@ -4,24 +4,13 @@ import std.stdio;
 import std.bitmanip;
 import std.conv;
 import std.concurrency;
+import std.array;
+import std.algorithm;
+import std.exception;
 
-shared StateHolder[State] stateMap;
+StateHolder[State] stateMap;
 
-void main() {
-	// use associative array shared StateNodeInfo[State]
-	//spawn(&doBFS!true, State(3,3,true)); // start at initial state
-	//spawn(&doBFS!false, State(0,0,false)); // start at goal state
-	writeln(State(3,3,true).expandState());
-}
-//template BoatSide(S) { 
-//		static if(S.isL) {
-//			alias LC LC;
-//			alias LM M;
-//		} else {
-//			alias RC LC;
-//			alias RM M;
-//		}
-//	}
+
 
 /**
 	State: # of missionairies (M) and cannibals (LC) on left and right side and the current location of the boat.
@@ -29,57 +18,26 @@ void main() {
 */
 struct State
 {
-	public: 
+	/*mixin(bitfields!(
+		ubyte,	"LC",	2,
+		ubyte,	"LM",	2,
+		bool,	"isL",	1,
+		ubyte,	"",		3	// padding
+	));*/
+	ubyte LC, LM;
+	bool isL;
 	
-	this(ubyte LC, ubyte LM, bool isL) { _LC=LC; _LM=LM; isL=isL; }
+	this(ubyte LC, ubyte LM, bool isL) { this.LC=LC; this.LM=LM; this.isL=isL; }
 	
-	// void functions?
-	@property LC() { return _LC; }
-	@property LC(const ubyte newLC) { return _LC = newLC; }
-	@property LM() { return _LM; }
-	@property LM(const ubyte newLM) { return _LM = newLM; }
-	@property RC() { return 3-_LC; }
-	@property ubyte RC(const ubyte newRC) { _LC = cast(ubyte)(3 - newRC); return newRC; }
-	@property RM() { return 3-_LM; }
-	@property RM(const ubyte newRM) { _LM = cast(ubyte)(3 - newRM); return newRM; }
-	@property isL() { return _isL; }
-	@property isL(bool newisL) { return _isL = newisL; }
-	
-	//bool isGoalState() const pure nothrow @safe { return _LC == 0 && _LM == 0; }
-	
-	const hash_t toHash() const pure nothrow @safe {
-		return _LC && _LM << 2 && _isL << 4;
-	}
-	
-	string toString() const { return "{LC:"~to!string(_LC)~",LM:"~to!string(_LM)~","~(_isL ? "L" : "R")~"}"; }
-	
-	@property bool isValid() const { return (_LM == 0 || _LM >= _LC) && (3-_LM == 0 || 3-_LM >= 3-_LC); }
-	
-	
+	string toString() const { return "{LC:"~to!string(LC)~",LM:"~to!string(LM)~","~(isL ? "L" : "R")~"}"; }
+	@property bool isValid() const { return (LM == 0 || LM >= LC) && (3-LM == 0 || 3-LM >= 3-LC); }
 	
 	State[] expandState() const {
+		assert(isValid);
 		State[] sx;
 		State news;
-		// actions: send M, LC, MM, MC, CC
-		//ubyte LC, M;
-		/*if (_isL) {
-			//LC = _LC; M = _LM;
-			alias LC LC;
-			alias LM M;
-		} else {
-			//LC = 3-_LC; M = 3-_LM;
-			alias RC LC;
-			alias RM M;
-		}*/
-		/*if (LC >= 2 && (3-M == 0 || 3-M == 1 && 3-LC == 0))
-			news = this; news.isL = !_isL;
-			news.LC = _LC - 2;*/
-		//alias BoatSide!(this).LC LC;
-		//function bool() LC = _isL ? this.LC : this.RC;
-		//auto LC = _isL ? _LC : 3-_LC;
-		
-		if (_isL) {
-			switch (this.LC) {
+		if (isL) {
+			switch (LC) {
 				case 3:
 				case 2:
 					news = this; news.isL = false;
@@ -87,7 +45,7 @@ struct State
 					if (news.isValid) sx ~= news;
 				case 1:
 					news = this; news.isL = false;
-					news.LC = LC-1;
+					news.LC -= 1;
 					if (news.isValid) sx ~= news;
 					if (LM >= 1) {
 						news = this; news.isL = false;
@@ -106,40 +64,40 @@ struct State
 					if (news.isValid) sx ~= news;
 				case 1:
 					news = this; news.isL = false;
-					news.LM = LM-1;
+					news.LM -= 1;
 					if (news.isValid) sx ~= news;
 				case 0: break;
 				default: assert(false);
 			}
-		} else {
-			switch (RC) {
+		} else { // boat is on the right
+			switch (3-LC) {
 				case 3:
 				case 2:
 					news = this; news.isL = true;
-					news.RC -= 2;
+					news.LC += 2;
 					if (news.isValid) sx ~= news;
 				case 1:
 					news = this; news.isL = true;
-					news.RC = RC-1;
+					news.LC += 1;
 					if (news.isValid) sx ~= news;
-					if (RM >= 1) {
+					if (3-LM >= 1) {
 						news = this; news.isL = true;
-						news.RC -= 1;
-						news.RM -= 1;
+						news.LC += 1;
+						news.LM += 1;
 						if (news.isValid) sx ~= news; 
 					}
 				case 0: break;
 				default: assert(false);
 			}
-			switch (RM) {
+			switch (3-LM) {
 				case 3:
 				case 2:
 					news = this; news.isL = true;
-					news.RM -= 2;
+					news.LM += 2;
 					if (news.isValid) sx ~= news;
 				case 1:
 					news = this; news.isL = true;
-					news.RM = RM-1;
+					news.LM += 1;
 					if (news.isValid) sx ~= news;
 				case 0: break;
 				default: assert(false);
@@ -149,79 +107,94 @@ struct State
 	}
 	
 	private:
-//	byte _LC=3, _LM=3;
-//	enum BoatLoc { L, R };
-//	BoatLoc boat = BoatLoc.L;
-//	bool _isL = 0;
-	mixin(bitfields!(
-		ubyte,	"_LC",	2,
-		ubyte,	"_LM",	2,
-		bool,	"_isL",	1,
-		ubyte,	"",		3	// padding
-	));
-	
-	
-	
-	invariant() {
-		assert(0 <= _LC && _LC <= 3);
-		assert(0 <= _LM && _LM <= 3);
-		//assert((_LM == 0 || _LM >= _LC) && (3-_LM == 0 || 3-_LM >= 3-_LC));
-	}
 	
 }
 
 class StateHolder 
 {
-	State sn;
+	State s;
 	StateHolder prev;
 	uint cost; // 1 more than the prev cost
 	bool isForward; // is it on the path going forward or backward?
 	
-	this(State sn, StateHolder prev, bool isForward) {
+	this(State s, StateHolder prev, bool isForward) {
 		uint cost = prev is null ? 0 : prev.cost+1;
-		this(sn, prev, cost, isForward);
+		this(s, prev, cost, isForward);
 	}
 	
-	private this(State sn, StateHolder prev, uint cost, bool isForward) {
-		this.sn = sn; this.prev = prev; this.cost = cost; this.isForward = isForward;
+	private this(State s, StateHolder prev, uint cost, bool isForward) {
+		this.s = s; this.prev = prev; this.cost = cost; this.isForward = isForward;
 	} 
 	
 	invariant() {
 		assert(prev is null || cost == prev.cost + 1); // path cost
 	}
 	
-	hash_t toHash() {
-		return sn.toHash();
+	/*hash_t toHash() {
+		return s.toHash();
 	}
 	
 	bool opEquals(Object rhs) {
 		auto that = cast(StateHolder) rhs;
-		return sn == that.sn;
-	}
+		return s == that.s;
+	}*/
 	
 	string toString() {
-		return "H"~sn.toString();
+		return "H"~s.toString();
 	}
 }
 
-void doBFS(bool isForward)(State startState)
+void main() {
+//	spawn(&doBFS!true, State(3,3,true)); // start at initial state
+//	spawn(&doBFS!false, State(0,0,false)); // start at goal state
+	//writeln(State(2,2,true).expandState());
+	
+	State[] frontierStart, frontierEnd, SS; // lifo queue - add to back, take from front
+	
+	// initialize frontiers and statemap
+	frontierStart ~= State(3,3,true);
+	frontierEnd ~= State(0,0,false);
+	stateMap[frontierStart[0]] = new StateHolder(frontierStart[0], null, true);
+	stateMap[frontierEnd[0]] = new StateHolder(frontierEnd[0], null, false);
+	writeln("first\n");
+	while (true) {
+		// expand start frontier
+		writeln("frontierStart before:", frontierStart);
+		if (!frontierStart.empty)
+			SS = doBFS!true(frontierStart);
+		if (SS) break;
+		writeln("frontierEnd   before:\t\t\t\t\t\t\t", frontierEnd);
+		if (!frontierEnd.empty)
+			SS = doBFS!false(frontierEnd);
+		if (SS) break;
+		assert(!frontierStart.empty || !frontierEnd.empty, "Empty frontiers"); // the problem is solvable
+	}
+	assert(SS);
+	writeln("SOLUTION: ",SS);
+}
+
+// returns solution sequence if we found one
+State[] doBFS(bool isForward)(ref State[] frontier)
 {
-	State[] frontier; // lifo queue - add to back, take from front
 	
-	frontier ~= startState;
-	stateMap[startState] = new StateHolder(startState, null, isForward);
-	
-	while (!frontier.empty) {
+	//while (!frontier.empty) {
 		State stateToExpand = frontier.front;
-		frontier = frontier.popFront();
+		frontier.popFront();
 		StateHolder stateToExpandHolder = stateMap[stateToExpand];
 		
-		foreach (State childState; expandState(stateToExpand)) {
+		foreach (State childState; stateToExpand.expandState()) {
 			if (childState in stateMap) {
 				// if it's from the other process, we met!
-				if (stateMap[childState].isForward != isForward) {
+				StateHolder childStateHolder = stateMap[childState];
+				if (childStateHolder.isForward != isForward) {
 					// we met!
-					// ...
+					writeln("GOT IT!");
+					State[] SS;
+					static if (isForward) {
+						SS = getSolutionSequence(stateToExpandHolder, childStateHolder);
+					} else {
+						SS = getSolutionSequence(childStateHolder, stateToExpandHolder);
+					}
 				} else {
 					// repeated state, throw it away
 					continue;
@@ -232,7 +205,20 @@ void doBFS(bool isForward)(State startState)
 				frontier ~= childState;
 			}
 		}
-	}
+	return null;
+}
+
+// initial <- ... <- lastForwardState -- firstBackwardState -> ... -> goal state
+State[] getSolutionSequence(StateHolder lastForwardState, StateHolder firstBackwardState)
+{
+	State[] SS = [lastForwardState.s]; // solution sequence
+	while (lastForwardState.prev !is null)
+		SS ~= (lastForwardState = lastForwardState.prev).s;
+	reverse(SS);
+	SS ~= firstBackwardState.s;
+	while (firstBackwardState !is null)
+		SS ~= (firstBackwardState = firstBackwardState.prev).s;
+	return SS;
 }
 
 
