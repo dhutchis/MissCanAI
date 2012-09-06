9df00fcf282e740917d6738e7ec637c96b846859
Dylan Hutchison <dhutchis@stevens.edu>
2012-09-06 16:29:11 -0400

    First Idea
    I got a bit too involved with templating again...

--- src/MissCanAI.d
+++ src/MissCanAI.d
@@ -5,19 +5,35 @@ import std.bitmanip;
 import std.conv;
 import std.concurrency;
 
+shared StateHolder[State] stateMap;
+
 void main() {
-	
-	
-	
+	// use associative array shared StateNodeInfo[State]
+	//spawn(&doBFS!true, State(3,3,true)); // start at initial state
+	//spawn(&doBFS!false, State(0,0,false)); // start at goal state
+	writeln(State(3,3,true).expandState());
 }
+//template BoatSide(S) { 
+//		static if(S.isL) {
+//			alias LC LC;
+//			alias LM M;
+//		} else {
+//			alias RC LC;
+//			alias RM M;
+//		}
+//	}
 
 /**
-	State: # of missionairies (M) and cannibals (C) on left and right side and the current location of the boat.
-		I only store the M and C on the left (LM, LC) as the right is computable from the left.
+	State: # of missionairies (M) and cannibals (LC) on left and right side and the current location of the boat.
+		I only store the M and LC on the left (LM, LC) as the right is computable from the left.
 */
-struct StateNode
+struct State
 {
-	public: // void functions?
+	public: 
+	
+	this(ubyte LC, ubyte LM, bool isL) { _LC=LC; _LM=LM; isL=isL; }
+	
+	// void functions?
 	@property LC() { return _LC; }
 	@property LC(const ubyte newLC) { return _LC = newLC; }
 	@property LM() { return _LM; }
@@ -29,13 +45,108 @@ struct StateNode
 	@property isL() { return _isL; }
 	@property isL(bool newisL) { return _isL = newisL; }
 	
-	bool isGoalState() const pure nothrow @safe { return _LC == 0 && _LM == 0; }
+	//bool isGoalState() const pure nothrow @safe { return _LC == 0 && _LM == 0; }
 	
-	hash_t toHash() const pure nothrow @safe {
+	const hash_t toHash() const pure nothrow @safe {
 		return _LC && _LM << 2 && _isL << 4;
 	}
 	
 	string toString() const { return "{LC:"~to!string(_LC)~",LM:"~to!string(_LM)~","~(_isL ? "L" : "R")~"}"; }
+	
+	@property bool isValid() const { return (_LM == 0 || _LM >= _LC) && (3-_LM == 0 || 3-_LM >= 3-_LC); }
+	
+	
+	
+	State[] expandState() const {
+		State[] sx;
+		State news;
+		// actions: send M, LC, MM, MC, CC
+		//ubyte LC, M;
+		/*if (_isL) {
+			//LC = _LC; M = _LM;
+			alias LC LC;
+			alias LM M;
+		} else {
+			//LC = 3-_LC; M = 3-_LM;
+			alias RC LC;
+			alias RM M;
+		}*/
+		/*if (LC >= 2 && (3-M == 0 || 3-M == 1 && 3-LC == 0))
+			news = this; news.isL = !_isL;
+			news.LC = _LC - 2;*/
+		//alias BoatSide!(this).LC LC;
+		//function bool() LC = _isL ? this.LC : this.RC;
+		//auto LC = _isL ? _LC : 3-_LC;
+		
+		if (_isL) {
+			switch (this.LC) {
+				case 3:
+				case 2:
+					news = this; news.isL = false;
+					news.LC -= 2;
+					if (news.isValid) sx ~= news;
+				case 1:
+					news = this; news.isL = false;
+					news.LC = LC-1;
+					if (news.isValid) sx ~= news;
+					if (LM >= 1) {
+						news = this; news.isL = false;
+						news.LC -= 1;
+						news.LM -= 1;
+						if (news.isValid) sx ~= news; 
+					}
+				case 0: break;
+				default: assert(false);
+			}
+			switch (LM) {
+				case 3:
+				case 2:
+					news = this; news.isL = false;
+					news.LM -= 2;
+					if (news.isValid) sx ~= news;
+				case 1:
+					news = this; news.isL = false;
+					news.LM = LM-1;
+					if (news.isValid) sx ~= news;
+				case 0: break;
+				default: assert(false);
+			}
+		} else {
+			switch (RC) {
+				case 3:
+				case 2:
+					news = this; news.isL = true;
+					news.RC -= 2;
+					if (news.isValid) sx ~= news;
+				case 1:
+					news = this; news.isL = true;
+					news.RC = RC-1;
+					if (news.isValid) sx ~= news;
+					if (RM >= 1) {
+						news = this; news.isL = true;
+						news.RC -= 1;
+						news.RM -= 1;
+						if (news.isValid) sx ~= news; 
+					}
+				case 0: break;
+				default: assert(false);
+			}
+			switch (RM) {
+				case 3:
+				case 2:
+					news = this; news.isL = true;
+					news.RM -= 2;
+					if (news.isValid) sx ~= news;
+				case 1:
+					news = this; news.isL = true;
+					news.RM = RM-1;
+					if (news.isValid) sx ~= news;
+				case 0: break;
+				default: assert(false);
+			}
+		}
+		return sx;
+	}
 	
 	private:
 //	byte _LC=3, _LM=3;
@@ -54,16 +165,26 @@ struct StateNode
 	invariant() {
 		assert(0 <= _LC && _LC <= 3);
 		assert(0 <= _LM && _LM <= 3);
+		//assert((_LM == 0 || _LM >= _LC) && (3-_LM == 0 || 3-_LM >= 3-_LC));
 	}
 	
 }
 
-class StateNodeHolder 
+class StateHolder 
 {
-	StateNode sn;
-	StateNodeHolder prev;
+	State sn;
+	StateHolder prev;
 	uint cost; // 1 more than the prev cost
 	bool isForward; // is it on the path going forward or backward?
+	
+	this(State sn, StateHolder prev, bool isForward) {
+		uint cost = prev is null ? 0 : prev.cost+1;
+		this(sn, prev, cost, isForward);
+	}
+	
+	private this(State sn, StateHolder prev, uint cost, bool isForward) {
+		this.sn = sn; this.prev = prev; this.cost = cost; this.isForward = isForward;
+	} 
 	
 	invariant() {
 		assert(prev is null || cost == prev.cost + 1); // path cost
@@ -74,7 +195,7 @@ class StateNodeHolder
 	}
 	
 	bool opEquals(Object rhs) {
-		auto that = cast(StateNodeHolder) rhs;
+		auto that = cast(StateHolder) rhs;
 		return sn == that.sn;
 	}
 	
@@ -82,3 +203,36 @@ class StateNodeHolder
 		return "H"~sn.toString();
 	}
 }
+
+void doBFS(bool isForward)(State startState)
+{
+	State[] frontier; // lifo queue - add to back, take from front
+	
+	frontier ~= startState;
+	stateMap[startState] = new StateHolder(startState, null, isForward);
+	
+	while (!frontier.empty) {
+		State stateToExpand = frontier.front;
+		frontier = frontier.popFront();
+		StateHolder stateToExpandHolder = stateMap[stateToExpand];
+		
+		foreach (State childState; expandState(stateToExpand)) {
+			if (childState in stateMap) {
+				// if it's from the other process, we met!
+				if (stateMap[childState].isForward != isForward) {
+					// we met!
+					// ...
+				} else {
+					// repeated state, throw it away
+					continue;
+				}
+			} else {
+				// new state - RACE HERE'
+				stateMap[childState] = new StateHolder(childState, stateToExpandHolder, isForward);
+				frontier ~= childState;
+			}
+		}
+	}
+}
+
+
